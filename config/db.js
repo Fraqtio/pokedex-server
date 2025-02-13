384109653d97f530e14769d096f809c9295c6a07
Fraqtio <fraqtio.invektiko@gmail.com>
2025-02-13 13:34:11 +0200

    no errors on my env

--- config/db.js
+++ config/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
         mongoose.connect(process.env.MONGO_URI)
             .then(() => console.log("MongoDB Connected"))
             .catch(err => console.error("MongoDB Connection Error:", err));
-        console.log('MongoDB Connected');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
