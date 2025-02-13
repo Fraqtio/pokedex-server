6f5bf6ae43de8092072af990918f28c44a7f2d27
Fraqtio <fraqtio.invektiko@gmail.com>
2025-02-13 13:24:22 +0200

    momgoose

--- config/db.js
+++ config/db.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        mongoose.connect(process.env.MONGO_URI)
+            .then(() => console.log("MongoDB Connected"))
+            .catch(err => console.error("MongoDB Connection Error:", err));
         console.log('MongoDB Connected');
     } catch (err) {
         console.error(err.message);
