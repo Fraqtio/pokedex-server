de370540ecf4e994ae41dc9421f1069b0128e9ff
Fraqtio <fraqtio.invektiko@gmail.com>
2025-02-13 13:25:44 +0200

    momgoose

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,7 +4,6 @@ const UserSchema = new mongoose.Schema({
     googleId: { type: String, unique: true },
     email: { type: String, unique: true, required: true },
     name: { type: String, required: true },
-    avatar: { type: String },
     favorites: [{ type: String }] // Список имен покемонов
 });
 
